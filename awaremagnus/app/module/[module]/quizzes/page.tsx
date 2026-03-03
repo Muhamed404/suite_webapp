588eb6494d13e96ba2b21f07a307ffb2e21c1985
Abdul Basit <basit@wenawa.com>
2026-03-03 21:09:18 +0500

    chore: add clsx utility for conditional class names

--- awaremagnus/app/module/[module]/quizzes/page.tsx
+++ awaremagnus/app/module/[module]/quizzes/page.tsx
@@ -5,6 +5,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { Button } from "@heroui/button";
 import { Search, ChevronRight, ChevronLeft, ArrowLeft } from "lucide-react";
+import clsx from "clsx";
 import { useRouter, useSearchParams } from "next/navigation";
 import { useQuery } from "@tanstack/react-query";
 
