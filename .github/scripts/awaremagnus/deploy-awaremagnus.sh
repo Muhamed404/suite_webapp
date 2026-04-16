fe921ad5f4626e5db665e11181a39942e3748b0a
wasiq-wenawa <s.wasiq@wenawa.com>
2026-04-16 15:10:18 +0500

    Update deploy-awaremagnus.sh

--- .github/scripts/awaremagnus/deploy-awaremagnus.sh
+++ .github/scripts/awaremagnus/deploy-awaremagnus.sh
@@ -100,6 +100,8 @@ cd "$DEPLOY_DIR"
 # Full install needed - devDependencies include build tools (TypeScript, Tailwind, etc.)
 npm install
 
+ls -lah.env
+
 # After build, prune dev dependencies to reduce deployment size
 # This is handled after Step 2 below
 
