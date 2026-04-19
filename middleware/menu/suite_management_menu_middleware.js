b29814a04c2018ab53c33913ba50a426672db7ca
Anonymous <asharniazi44@gmail.com>
2026-04-20 02:03:26 +0300

    Update suite_management_menu_middleware.js

--- middleware/menu/suite_management_menu_middleware.js
+++ middleware/menu/suite_management_menu_middleware.js
@@ -43,13 +43,6 @@ function generateSuiteManagementMenu(req, organization) {
       allowedRoles: [enums.userType.MagSuperAdmin, enums.userType.MagSubAdmin]
     },
     {
-      module: "Bulk import jobs",
-      labelKey: "menu.suite.bulkImportJobs",
-      link: "/user/bulk-import/jobs",
-      img_path: "/securemagnus_2025/images/icons/primary-uses.svg",
-      allowedRoles: [enums.userType.MagSuperAdmin, enums.userType.MagSubAdmin]
-    },
-    {
       module: "Categories",
       labelKey: "menu.suite.cybersecurityCategories",
       link: "/cybersecurity/categories/list",
